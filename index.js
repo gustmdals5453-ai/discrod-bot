bab41e90dfad6c519830c23d2151f4d100b5e40b
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-27 16:51:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const client = new Client({
   ]
 });
 
-// 웹서버
+// 🔥 에러 로그 강제 출력 (디버그용)
+process.on("uncaughtException", console.error);
+process.on("unhandledRejection", console.error);
+
+// 웹서버 (렌더용)
 http.createServer((req,res)=>{
   res.writeHead(200);
   res.end("Bot running");
@@ -20,14 +24,19 @@ http.createServer((req,res)=>{
 // DB
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("DB 연결됨"))
-.catch(console.error);
+.catch(err=>{
+  console.error("DB 오류:", err);
+  process.exit(1);
+});
 
 // 명령어 로드
 require("./핸들러/명령어로더")(client);
 
 // 이벤트
 client.on("messageCreate", require("./이벤트/메시지"));
-client.on("interactionCreate", require("./이벤트/인터랙션"));
+
+// ❌ 인터랙션 파일 없어서 터지던 부분 제거
+// client.on("interactionCreate", require("./이벤트/인터랙션"));
 
 client.once("ready", ()=>console.log("봇 준비됨"));
 
