0855a26503e6dab0ba0e64063af6e1bba62305bb
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-28 01:55:21 +0900

    Update 블랙잭.js

--- 명령어/블랙잭.js
+++ 명령어/블랙잭.js
@@ -1,29 +1,24 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
 module.exports = {
   name:"블랙잭",
 
-  async execute(m,args,{user,E,G,f}){
+  async execute(m,args,{user,E,err}){
 
     const bet=Number(args[0]);
+
     if(isNaN(bet)||user.money<bet)
-      return m.reply({embeds:[E("오류",0xFF4D4D)]});
+      return m.reply(err(E,"금액 오류 또는 잔액 부족"));
 
-    let msg=await m.reply({embeds:[G("블랙잭",true).setDescription("🃏 카드 배분 중...")]});
-
-    await new Promise(r=>setTimeout(r,1000));
-
-    const userScore=Math.floor(Math.random()*11)+11;
-    const botScore=Math.floor(Math.random()*11)+11;
-
-    let win=userScore>botScore;
-    let change=win?bet:-bet;
-
-    user.money+=change;
-    await user.save();
-
-    return msg.edit({
-      embeds:[
-        G("결과",win)
-        .setDescription(`플레이어: ${userScore}\n딜러: ${botScore}\n${change>0?"+":""}${f(change)}원\n잔액 ${f(user.money)}원`)
+    return m.reply({
+      embeds:[E("블랙잭").setDescription(`배팅 ${bet}원`)],
+      components:[
+        new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+          .setCustomId(`game_블랙잭_${bet}`)
+          .setLabel("🃏 시작")
+          .setStyle(ButtonStyle.Primary)
+        )
       ]
     });
   }
