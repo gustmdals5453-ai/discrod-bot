1122d1b664188c7a8f15d4bfcf18120efffb91bd
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-28 02:29:00 +0900

    Update 송금.js

--- 명령어/송금.js
+++ 명령어/송금.js
@@ -1,33 +1,45 @@
 module.exports = {
-  name:"송금",
+  name: "송금",
 
-  async execute(m,args,{user,getUser,E,f,err}){
+  async execute(m, args, { user, getUser, E, err, f }) {
 
-    const 대상=m.mentions.users.first();
-    const 금액=Number(args.find(a=>!isNaN(a)));
+    const 대상 = m.mentions.users.first();
+    const 금액 = Number(args.find(a => !isNaN(a)));
 
-    if(!대상||isNaN(금액)||금액<=0)
-      return m.reply(err(E,"형식: !송금 @유저 금액"));
+    if (!대상 || isNaN(금액) || 금액 <= 0)
+      return m.reply(err(E, "형식: !송금 @유저 금액"));
 
-    if(대상.id===m.author.id)
-      return m.reply(err(E,"자기 자신에게는 송금할 수 없음"));
+    if (대상.id === m.author.id)
+      return m.reply(err(E, "자기 자신에게 송금할 수 없습니다"));
 
-    if(user.money<금액)
-      return m.reply(err(E,"잔액이 부족합니다"));
+    if (user.money < 금액)
+      return m.reply(err(E, "잔액 부족"));
 
-    const u=await getUser(대상.id);
+    const 대상유저 = await getUser(대상.id);
 
-    user.money-=금액;
-    u.money+=금액;
+    user.money -= 금액;
+    대상유저.money += 금액;
 
     await user.save();
-    await u.save();
+    await 대상유저.save();
 
     return m.reply({
-      embeds:[
-        E("송금 완료")
-        .setDescription(`${대상} ${f(금액)}원\n잔액 ${f(user.money)}원`)
+      embeds: [
+        E("송금 완료").setDescription(
+`## 💸 송금 완료
+
+~~~diff
++ ${f(금액)}원 전송
+~~~
+
+## 👤 대상
+<@${대상.id}>
+
+## 💰 잔액
+${f(user.money)}원`
+        )
       ]
     });
+
   }
 };
