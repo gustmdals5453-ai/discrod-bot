d401fbcb7c24f61ba29a143ef0ee987841a69cff
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-27 23:24:12 +0900

    Create 랭킹.js

---
+++ 명령어/랭킹.js
@@ -0,0 +1,19 @@
+const User = require("../모델/유저");
+
+module.exports = {
+  name:"랭킹",
+
+  async execute(m,args,{E,f}){
+
+    const top=await User.find().sort({money:-1}).limit(10);
+
+    return m.reply({
+      embeds:[
+        E("랭킹")
+        .setDescription(
+          top.map((u,i)=>`${i+1}위 <@${u.userId}> ${f(u.money)}원`).join("\n")
+        )
+      ]
+    });
+  }
+};
