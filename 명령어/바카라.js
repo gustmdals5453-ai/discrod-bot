9fe44551a8994cedf90773c3ee83063f15e84a06
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-28 02:26:16 +0900

    Update 바카라.js

--- 명령어/바카라.js
+++ 명령어/바카라.js
@@ -7,14 +7,19 @@ module.exports = {
 
     const bet = Number(args[0]);
 
-    if(isNaN(bet) || bet <= 0)
+    if(isNaN(bet))
       return m.reply(err(E,"금액 입력"));
 
     if(user.money < bet)
       return m.reply(err(E,"잔액 부족"));
 
     return m.reply({
-      embeds:[E("바카라").setDescription(`\`\`\`\n배팅: ${bet}원\n\`\`\``)],
+      embeds:[E("바카라").setDescription(
+`~~~diff
+# 바카라
++ 배팅: ${bet}원
+~~~`
+      )],
       components:[
         new ActionRowBuilder().addComponents(
           new ButtonBuilder().setCustomId(`game_player_${bet}`).setLabel("플레이어").setStyle(ButtonStyle.Primary),
