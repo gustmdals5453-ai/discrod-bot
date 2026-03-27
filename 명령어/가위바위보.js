77f6a8f007e1f3e47fc60f73cdbfd4d3b4fee185
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-28 02:26:06 +0900

    Update 가위바위보.js

--- 명령어/가위바위보.js
+++ 명령어/가위바위보.js
@@ -7,14 +7,19 @@ module.exports = {
 
     const bet = Number(args[0]);
 
-    if(isNaN(bet) || bet <= 0)
+    if(isNaN(bet))
       return m.reply(err(E,"금액 입력"));
 
     if(user.money < bet)
       return m.reply(err(E,"잔액 부족"));
 
     return m.reply({
-      embeds:[E("가위바위보").setDescription(`\`\`\`\n배팅: ${bet}원\n\`\`\``)],
+      embeds:[E("가위바위보").setDescription(
+`~~~diff
+# 가위바위보
++ 배팅: ${bet}원
+~~~`
+      )],
       components:[
         new ActionRowBuilder().addComponents(
           new ButtonBuilder().setCustomId(`rps_${bet}_가위`).setLabel("가위").setStyle(ButtonStyle.Primary),
