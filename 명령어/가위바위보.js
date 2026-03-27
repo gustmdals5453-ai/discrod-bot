f87439994407ea3d5a5173714f2591aab45b167e
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-28 02:36:47 +0900

    Update 가위바위보.js

--- 명령어/가위바위보.js
+++ 명령어/가위바위보.js
@@ -3,23 +3,28 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 module.exports = {
   name:"가위바위보",
 
-  async execute(m,args,{user,E,err}){
+  async execute(m,args,{user,E,err,f}){
 
     const bet = Number(args[0]);
 
-    if(isNaN(bet))
-      return m.reply(err(E,"금액 입력"));
+    if(isNaN(bet) || bet <= 0)
+      return m.reply(err(E,"금액을 입력해주세요"));
 
     if(user.money < bet)
       return m.reply(err(E,"잔액 부족"));
 
     return m.reply({
-      embeds:[E("가위바위보").setDescription(
-`~~~diff
-# 가위바위보
-+ 배팅: ${bet}원
-~~~`
-      )],
+      embeds:[
+        E("가위바위보").setDescription(
+`## ✌️ 가위바위보
+
+\`\`\`diff
++ 배팅 금액: ${f(bet)}원
+\`\`\`
+
+## 선택하세요`
+        )
+      ],
       components:[
         new ActionRowBuilder().addComponents(
           new ButtonBuilder().setCustomId(`rps_${bet}_가위`).setLabel("가위").setStyle(ButtonStyle.Primary),
