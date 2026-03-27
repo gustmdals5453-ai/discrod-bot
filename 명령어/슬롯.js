75c0a838b855c3d090bc0c61a2b4ef18738f887a
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-28 01:54:54 +0900

    Update 슬롯.js

--- 명령어/슬롯.js
+++ 명령어/슬롯.js
@@ -1,7 +1,9 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
 module.exports = {
   name:"슬롯",
 
-  async execute(m,args,{user,E,G,f,rand,err}){
+  async execute(m,args,{user,E,err}){
 
     const bet=Number(args[0]);
 
@@ -11,40 +13,15 @@ module.exports = {
     if(user.money<bet)
       return m.reply(err(E,"잔액이 부족합니다"));
 
-    const icons=["🍒","🍋","🍊","⭐","💎"];
-
-    let msg=await m.reply({embeds:[G("슬롯",true).setDescription("🎰 돌리는 중...")]});
-
-    for(let i=0;i<3;i++){
-      await new Promise(r=>setTimeout(r,600));
-      await msg.edit({
-        embeds:[G("슬롯",true)
-        .setDescription(`${rand(icons)} | ${rand(icons)} | ${rand(icons)}`)]
-      });
-    }
-
-    const r1=rand(icons),r2=rand(icons),r3=rand(icons);
-
-    let change=0;
-    let win=false;
-
-    if(r1===r2&&r2===r3){
-      change=bet*5;
-      win=true;
-    }
-    else if(r1===r2||r2===r3||r1===r3){
-      change=bet*2;
-      win=true;
-    }
-    else change=-bet;
-
-    user.money+=change;
-    await user.save();
-
-    return msg.edit({
-      embeds:[
-        G("결과",win)
-        .setDescription(`${r1} | ${r2} | ${r3}\n${change>0?"+":""}${f(change)}원\n잔액 ${f(user.money)}원`)
+    return m.reply({
+      embeds:[E("슬롯").setDescription(`배팅 ${bet}원`)],
+      components:[
+        new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+          .setCustomId(`slot_${bet}`)
+          .setLabel("🎰 시작")
+          .setStyle(ButtonStyle.Success)
+        )
       ]
     });
   }
