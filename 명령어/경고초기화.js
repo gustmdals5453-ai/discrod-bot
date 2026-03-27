ca39839d695d125f0f9e59ae57087369c69dd084
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-27 23:30:44 +0900

    Update 경고초기화.js

--- 명령어/경고초기화.js
+++ 명령어/경고초기화.js
@@ -1,16 +1,17 @@
 module.exports = {
   name:"경고초기화",
 
-  async execute(m,args,{getUser,E}){
+  async execute(m,args,{getUser,E,err}){
 
     if(!m.member.permissions.has("Administrator"))
-      return m.reply({embeds:[E("오류",0xFF4D4D)]});
+      return m.reply(err(E,"관리자만 사용 가능합니다"));
 
     const 대상=m.mentions.users.first();
     if(!대상)
-      return m.reply({embeds:[E("오류",0xFF4D4D)]});
+      return m.reply(err(E,"유저를 지정해주세요"));
 
     const u=await getUser(대상.id);
+
     u.warns=0;
     u.warnList=[];
 
