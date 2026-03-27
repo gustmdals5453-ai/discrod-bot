7fcd7a41ad7e1fe450b8a9cf3bbe5005f35ea6c1
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-27 23:30:03 +0900

    Update 돈줘.js

--- 명령어/돈줘.js
+++ 명령어/돈줘.js
@@ -1,15 +1,13 @@
 module.exports = {
   name:"돈줘",
 
-  async execute(m,args,{user,E,f}){
+  async execute(m,args,{user,E,f,err}){
 
     const now=new Date();
     const last=new Date(user.lastDaily);
 
     if(now.toDateString()===last.toDateString())
-      return m.reply({
-        embeds:[E("제한",0xFF4D4D).setDescription("하루 1회만 가능")]
-      });
+      return m.reply(err(E,"하루 1회만 가능합니다"));
 
     user.lastDaily=Date.now();
     user.money+=10000;
