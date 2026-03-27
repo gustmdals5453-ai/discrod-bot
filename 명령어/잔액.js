82edbd5598a947f727acdbf1124edd52b2a7ac8b
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-28 02:46:13 +0900

    Update 잔액.js

--- 명령어/잔액.js
+++ 명령어/잔액.js
@@ -2,8 +2,18 @@ module.exports = {
   name:"잔액",
 
   async execute(m,args,{user,E,f}){
+
     return m.reply({
-      embeds:[E("잔액").setDescription(`${f(user.money)}원`)]
+      embeds:[
+        E("잔액").setDescription(
+`## 💰 현재 잔액
+
+\`\`\`diff
++ ${f(user.money)}원
+\`\`\``
+        )
+      ]
     });
+
   }
 };
