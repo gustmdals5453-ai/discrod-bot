84328dd217fd3068614cd26fe4efe2483e6d283f
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-27 23:10:10 +0900

    Update 임베드.js

--- 유틸/임베드.js
+++ 유틸/임베드.js
@@ -1,17 +1,17 @@
 const { EmbedBuilder } = require("discord.js");
 
-exports.기본 = (t,c=0x00E5FF)=>
-  new EmbedBuilder()
-  .setColor(c)
-  .setAuthor({name:"한국협회"})
-  .setTitle(`┏━━━ ${t} ━━━┓`)
-  .setFooter({text:"한국협회 시스템"})
-  .setTimestamp();
+exports.E = (t,c=0x00E5FF)=>
+new EmbedBuilder()
+.setColor(c)
+.setAuthor({name:"한국협회"})
+.setTitle(`┏━━━ ${t} ━━━┓`)
+.setFooter({text:"한국협회 시스템"})
+.setTimestamp();
 
-exports.카지노 = (t,c=0xFFD700)=>
-  new EmbedBuilder()
-  .setColor(c)
-  .setAuthor({name:"한국협회 카지노"})
-  .setTitle(`┏━━━ ${t} ━━━┓`)
-  .setFooter({text:"한국협회 카지노"})
-  .setTimestamp();
+exports.G = (t,win)=>
+new EmbedBuilder()
+.setColor(win?0x00FF88:0xFF4D4D)
+.setAuthor({name:"한국협회 카지노"})
+.setTitle(`┏━━━ ${t} ━━━┓`)
+.setFooter({text:"카지노"})
+.setTimestamp();
