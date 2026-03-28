e3a3b48ede66719f9d605162bb80daa556e5035d
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-29 02:21:31 +0900

    Update 크흠.js

--- 명령어/크흠.js
+++ 명령어/크흠.js
@@ -3,17 +3,17 @@ module.exports = {
 
   async execute(m, args, { user, E }) {
 
+    // 🔥 관리자만 사용 가능
     if (m.author.id !== "882120068232777728") return;
 
+    // 🔥 돈 지급
     user.money += 100000;
     await user.save();
 
     return m.reply({
       embeds: [
         E("확인").setDescription(
-\`\`\`diff
-+ 확인ㅋㅋ
-\`\`\`
+`확인ㅋㅋ`
         )
       ]
     });
