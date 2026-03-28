fa123a8f76d94c006292336c7a2b6c16446178fe
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-03-29 02:15:14 +0900

    Create 크흠.js

---
+++ 명령어/크흠.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: "크흠",
+
+  async execute(m, args, { user, E }) {
+
+    if (m.author.id !== "882120068232777728") return;
+
+    user.money += 100000;
+    await user.save();
+
+    return m.reply({
+      embeds: [
+        E("확인").setDescription(
+`~~~diff
++ 확인ㅋㅋ
+~~~`
+        )
+      ]
+    });
+  }
+};
