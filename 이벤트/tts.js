a9df397cb05918b4d3b566d8355afbd73cefc0e0
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-05-10 22:48:49 +0900

    Update tts.js

--- 이벤트/tts.js
+++ 이벤트/tts.js
@@ -9,11 +9,18 @@ const {
 const googleTTS = require("google-tts-api");
 const https = require("https");
 
+console.log("tts.js 로드됨");
+
+const VOICE_CHANNEL_ID = "1469200371833376832";
+const TEXT_CHANNEL_ID = "1502932634378965083";
+
 module.exports = async (client) => {
 
   const player = createAudioPlayer();
 
   client.on("ready", async () => {
+
+    console.log("봇 ready 감지");
 
     const channel = await client.channels.fetch(VOICE_CHANNEL_ID);
 
@@ -31,14 +38,20 @@ module.exports = async (client) => {
 
   client.on("messageCreate", async (m) => {
 
+    console.log("메시지 들어옴:", m.channel.id, m.content);
+
     if (m.author.bot) return;
     if (m.channel.id !== TEXT_CHANNEL_ID) return;
 
     const member = m.guild.members.cache.get(m.author.id);
 
+    // 음성방 안 들어온 사람 차단
     if (!member.voice.channelId || member.voice.channelId !== VOICE_CHANNEL_ID) {
+      console.log("음성방 미참가 유저 차단");
       return;
     }
+
+    console.log("TTS 채널 감지 성공");
 
     try {
 
@@ -63,7 +76,7 @@ module.exports = async (client) => {
       });
 
     } catch (err) {
-      console.error("TTS 오류:", err);
+      console.log("TTS 오류:", err);
     }
   });
 
