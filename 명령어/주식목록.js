de040f9cc5788ef2c73a2deed64a8ea9816e3386
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-05-10 21:46:49 +0900

    Create 주식목록.js

---
+++ 명령어/주식목록.js
@@ -0,0 +1,35 @@
+const { EmbedBuilder } = require("discord.js");
+const Stock = require("../모델/주식");
+
+module.exports = {
+  이름: "주식목록",
+
+  async 실행(message) {
+
+    const stocks = await Stock.find();
+
+    const embed = new EmbedBuilder()
+      .setTitle("📈 주식 목록")
+      .setColor("Blue");
+
+    stocks.forEach(stock => {
+
+      const emoji =
+        stock.change > 0 ? "📈" :
+        stock.change < 0 ? "📉" :
+        "➖";
+
+      embed.addFields({
+        name: `${emoji} ${stock.name} (${stock.code})`,
+        value:
+`현재가: ${stock.price.toLocaleString()}원
+변동률: ${stock.change}%`,
+        inline: false
+      });
+    });
+
+    message.reply({
+      embeds: [embed]
+    });
+  }
+};
