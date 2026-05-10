f703b5137ae2369f6cdf980ab29f37da2966ec18
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-05-10 16:37:05 +0900

    Update tts.js

--- 이벤트/tts.js
+++ 이벤트/tts.js
@@ -10,6 +10,8 @@ const fs = require("fs");
 const path = require("path");
 const https = require("https");
 
+console.log("tts.js 로드됨");
+
 const VOICE_CHANNEL_ID = "1469200371833376832";
 const TEXT_CHANNEL_ID = "1502932634378965083";
 
@@ -18,6 +20,8 @@ module.exports = async (client) => {
   const player = createAudioPlayer();
 
   client.on("ready", async () => {
+    console.log("봇 ready 감지");
+
     const channel = await client.channels.fetch(VOICE_CHANNEL_ID);
 
     const connection = joinVoiceChannel({
@@ -32,10 +36,21 @@ module.exports = async (client) => {
   });
 
   client.on("messageCreate", async (m) => {
+
+    console.log("메시지 들어옴:", m.channel.id, m.content);
+
     if (m.author.bot) return;
     if (m.channel.id !== TEXT_CHANNEL_ID) return;
 
-    console.log("메시지 감지:", m.content);
+    const member = m.guild.members.cache.get(m.author.id);
+
+    // 🔥 음성방 안 들어온 사람 차단
+    if (!member.voice.channelId || member.voice.channelId !== VOICE_CHANNEL_ID) {
+      console.log("음성방 미참가 유저 차단");
+      return;
+    }
+
+    console.log("TTS 채널 감지 성공");
 
     try {
       const url = googleTTS.getAudioUrl(m.content, {
@@ -52,7 +67,11 @@ module.exports = async (client) => {
         stream.on("finish", () => {
           console.log("mp3 생성 완료");
 
-          const resource = createAudioResource(file);
+          const resource = createAudioResource(file, {
+            inlineVolume: true
+          });
+
+          resource.volume.setVolume(1);
 
           player.play(resource);
 
