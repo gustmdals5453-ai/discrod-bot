dba5cee2f162d8818e09a1be4e034834a1189c2e
gustmdals5453-ai <gustmdals5453@gmail.com>
2026-05-10 21:51:00 +0900

    Update 유저.js

--- 모델/유저.js
+++ 모델/유저.js
@@ -2,10 +2,41 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
   userId:String,
-  money:{type:Number,default:0},
-  lastDaily:{type:Number,default:0},
-  warns:{type:Number,default:0},
-  warnList:{type:Array,default:[]}
+
+  money:{
+    type:Number,
+    default:0
+  },
+
+  lastDaily:{
+    type:Number,
+    default:0
+  },
+
+  warns:{
+    type:Number,
+    default:0
+  },
+
+  warnList:{
+    type:Array,
+    default:[]
+  },
+
+  // ================== 주식 ==================
+
+  stocks:{
+    type:Object,
+    default:{}
+  },
+
+  // ================== 회사 ==================
+
+  company:{
+    type:String,
+    default:null
+  }
+
 });
 
 module.exports = mongoose.model("User",schema);
